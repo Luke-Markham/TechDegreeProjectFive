1677421637ddb48d721cac703ee8098973f92821
Luke_Markham <lukeeliotmarkham2@hotmail.com>
2019-04-23 21:06:35 +0700

    refactored code

--- photo_gallery_v4/JS/app.js
+++ photo_gallery_v4/JS/app.js
@@ -5,39 +5,6 @@ lightbox.option({
   maxWidth: 700
 });
 
-// This is jquery I can log what I write in the search and also loops through the captions and console log them
-// However I don't really understand whats going on I just sort of stumbled my way into this
-// at the if statement things go wierd, I know I need to swithc caption and search around in let "searchMatch"
-// but then I get an error.
-
-// let search = $(".search-bar")
-//   .on()
-//   .keyup(function() {
-//     search = $(".search-bar")
-//       .val()
-//       .toLowerCase();
-//     console.log(search);
-//   });
-
-// let anchorTag = $(".main-content a");
-
-// for (var i = 0; i < anchorTag.length; i++) {
-//   let caption = anchorTag[i].getAttribute("data-title").toLowerCase();
-//   console.log(caption);
-// }
-
-//   let searchMatch = search.includes(caption);
-//   console.log(searchMatch);
-// }
-//   if (searchMatch) {
-//     anchorTag.css("display", "");
-//   } else {
-//     // anchorTag.css("display", "none");
-//   }
-// }
-
-// WHY DOES THIS NOT WORK ? always get undefined error  ---------------------------------
-
 document.querySelector(".search-bar").addEventListener("keyup", searchFunction);
 
 function searchFunction() {
